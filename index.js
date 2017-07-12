c574dbd1e1fa32b37beceb54d42fb722247bb4a2
Titus Wormer <tituswormer@gmail.com>
2017-07-12 21:46:30 +0200

    Remove superfluous comments

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 'use strict';
 
-/* Expose. */
 module.exports = cept;
 
-/**
- * Intercept calls to `name` on `context`.
- *
- * @param {Object} context
- * @param {string} name
- * @param {Function} callback
- * @return {Function} A method to stop intercepting.
- */
 function cept(context, name, callback) {
   var original = context[name];
 
--- test.js
+++ test.js
@@ -1,10 +1,8 @@
 'use strict';
 
-/* Dependencies. */
 var test = require('tape');
 var cept = require('./');
 
-/* Tests. */
 test('cept(context, key, callback)', function (t) {
   var original = process.cwd();
   var stop = cept(process, 'cwd', fake);
