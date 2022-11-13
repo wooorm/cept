365fba529a918ef29ac974c30673337bccf86d04
Titus Wormer <tituswormer@gmail.com>
2022-11-13 20:21:02 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,18 +1,17 @@
+import assert from 'node:assert/strict'
 import process from 'node:process'
-import test from 'tape'
+import test from 'node:test'
 import {cept} from './index.js'
 
-test('cept(context, key, callback)', function (t) {
+test('cept(context, key, callback)', function () {
   const original = process.cwd()
   const stop = cept(process, 'cwd', fake)
 
-  t.equal(process.cwd(), fake())
+  assert.equal(process.cwd(), fake())
 
   stop()
 
-  t.equal(process.cwd(), original)
-
-  t.end()
+  assert.equal(process.cwd(), original)
 })
 
 function fake() {
